3a2f2b53113eb7f8e272e67fc0b1f924c789d3b0
Darian Weingaertner <darian.weingaertner@gmail.com>
2024-04-10 12:55:16 +0200

    first push

---
+++ src/main/java/com/example/PutzPlaner/PutzPlanerApplication.java
@@ -0,0 +1,13 @@
+package com.example.PutzPlaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PutzPlanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PutzPlanerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PutzPlaner/PutzPlanerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PutzPlaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PutzPlanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
